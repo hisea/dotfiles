e4a9a7a2fa5cfbcdcda1c0981e6aa4759233b432
Yinghai Zhao <zyinghai@gmail.com>
2023-12-23 16:00:11 -0500

    update nvim config.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -15,9 +15,23 @@ require("lazy").setup({
   "williamboman/mason.nvim",
   'neovim/nvim-lspconfig',
   "williamboman/mason-lspconfig.nvim",
-  "mhartington/oceanic-next",
+  "roflolilolmao/oceanic-next.nvim",
   "folke/which-key.nvim",
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+  'mfussenegger/nvim-dap',
+  'nvim-lua/plenary.nvim',
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    -- build = "make install_jsregexp"
+  },
   {
     'numToStr/Comment.nvim',
     opts = {
@@ -84,6 +98,7 @@ require('lualine').setup({
     theme = 'OceanicNext',
     component_separators = '',
     section_separators = { left = '', right = ''},
+    -- symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
   }
 })
 
@@ -99,15 +114,66 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = { "rust", "lua", "vim", "vimdoc" },
 }
 
-require("rust-tools").setup({
+local rt = require("rust-tools")
+
+rt.setup({
   server = {
     on_attach = function(_, bufnr)
       -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>rr", rt.hover_actions.hover_actions, { buffer = bufnr })
       -- Code action groups
       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
+  tools = {
+	  hover_actions = {
+		  -- the border that is used for the hover window
+		  -- see vim.api.nvim_open_win()
+		  border = {
+			  { "╭", "NormalFloat" },
+			  { "─", "NormalFloat" },
+			  { "╮", "NormalFloat" },
+			  { "│", "NormalFloat" },
+			  { "╯", "NormalFloat" },
+			  { "─", "NormalFloat" },
+			  { "╰", "NormalFloat" },
+			  { "│", "NormalFloat" },
+		  }
+	  },
+  }
+})
+
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
 })
 
 -- Colorscheme
