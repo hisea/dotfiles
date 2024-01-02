31ee293d1fa650f9cd193c5e78284e2e24e6d0b8
Yinghai Zhao <yinghai.zhao@shopify.com>
2024-01-02 11:52:40 -0500

    Fix warnings for Trouble.

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -4,7 +4,7 @@ return  {
     require("trouble").setup({
       position = "bottom",
       icons = true,
-      mode = "lsp_document_diagnostics",
+      mode = "document_diagnostics",
     })
     vim.keymap.set('n', "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>",{ desc = "Document Diagnostics (Trouble)" })
     vim.keymap.set('n', "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", { desc = "Workspace Diagnostics (Trouble)"})
